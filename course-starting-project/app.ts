c4c16d8ebfa693eb1c3402fd0b4f5818cf8bcf35
jxuho <jxuholee@gmail.com>
2024-04-18 21:37:32 +0900

    TS type - function

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -8,3 +8,17 @@ function printResult(num:number) {
 
 console.log(printResult(add(5,12)));  // undefined
 printResult(add(5, 12));  // Result: 17
+
+
+// function type
+let combineValues: Function;
+
+console.log(add(8,8));
+
+
+// 함수의 인자와 return값의 type을 정함.
+let combineValues2: (a: number, b: number) => number;
+
+combineValues2 = add;
+combineValues2 = printResult;
+// combineValues = 5;
