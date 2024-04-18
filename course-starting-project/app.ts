931ce6693a147269013c51a841d818b8bff544ea
jxuho <jxuholee@gmail.com>
2024-04-18 14:57:33 +0900

    TS type - object

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,9 +1,27 @@
-function add (n1: number, n2: number) {
-  return n1 + n2;
+// TS가 type infer함. => 가장 권장할만 함.
+const person1 = {
+  name: "Juho",
+  age: 26
 }
+console.log(person1.name);
 
-const number1 = 5;
-const number2 = 2.8;
 
-const result = add(number1, number2);
-console.log(result); 
+
+// person2가 object라는 것만 지정했기 때문에, property에 접근할 때 오류 출력함.
+const person2: object = {
+  name: "Juho",
+  age: 26
+}
+console.log(person2.name);
+
+
+
+// person3 object의 key-type을 지정
+const person3: {
+  name: string;
+  age: number;
+} = {
+  name: "Juho",
+  age: 26
+}
+console.log(person3.name);
