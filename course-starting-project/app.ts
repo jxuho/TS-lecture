9ee7aeff018a348fe53d961432ff3ebd36dd905e
jxuho <jxuholee@gmail.com>
2024-04-18 15:16:14 +0900

    TS type - array

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,27 +1,20 @@
-// TS가 type infer함. => 가장 권장할만 함.
 const person1 = {
   name: "Juho",
-  age: 26
+  age: 26,
+  hobbies: ["sports", "cooking"]
 }
+
+let favoriteActivites1: string[];
+favoriteActivites1 = ["Sports", 1];
+
+let favoriteActivites2: any[];
+favoriteActivites2 = ["Sports", 1];
+
+
 console.log(person1.name);
 
 
-
-// person2가 object라는 것만 지정했기 때문에, property에 접근할 때 오류 출력함.
-const person2: object = {
-  name: "Juho",
-  age: 26
+for (const hobby of person1.hobbies) {
+  console.log(hobby.toUpperCase());
 }
-console.log(person2.name);
 
-
-
-// person3 object의 key-type을 지정. 가능하지만 불필요함.
-const person3: {
-  name: string;
-  age: number;
-} = {
-  name: "Juho",
-  age: 26
-}
-console.log(person3.name);
