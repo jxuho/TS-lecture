b7131b9bf0a17c0334e1db97081a58ed4ceaf4ad
jxuho <jxuholee@gmail.com>
2024-04-21 15:33:10 +0900

    JS class

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -20,3 +20,6 @@ console.log("end");
 const accountingCopy = { describe: accounting.describe };
 
 accountingCopy.describe();
+// 위 코드에서 describe는 accounting객체의 describe method 주소를 가르킨다(포인터)
+// 하지만, this는 일반적으로 해당 method를 호출한 대상을 의미하고, 여기서는 accountingCopy를 가르킨다.
+// 하지만, accountingCopy를 정의할 때, name property를 정의하지 않았으므로, 'Department: undefined'가 출력된다.
