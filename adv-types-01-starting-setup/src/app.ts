554608ef6c828529c52828757d1f29939c954a30
jxuho <jxuholee@gmail.com>
2024-04-27 22:40:43 +0900

    TS generic

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -3,7 +3,6 @@ type Admin = {
   privileges: string[];
 };
 
-
 type Employee = {
   name: string;
   startDate: Date;
@@ -13,26 +12,54 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "Juho",
-  privileges: ['create-server'],
-  startDate: new Date() 
-}
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
 
 type Combinable = string | number;
 type Numeric = number | boolean;
 
-
 type Universal = Combinable & Numeric;
 
-
-function add(a: number, b: number): number
-function add(a: string, b: string): string
-function add(a: string, b: number): string
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
 function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string")  {
+  if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
 
-const result = add('Juho', 'Lee');
-result.split("")
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return { ...objA, ...objB };
+}
+
+const mergedObj = merge({ name: "juho" }, { age: 26 }); // const mergedObj: object
+
+const mergedObj2 = merge({ name: "juho", age: 26 }, { hobby: "photo" });
+
+console.log(mergedObj.name);
+console.log(mergedObj2.hobby);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length > 0) {
+    descriptionText = "Got " + element.length + "elements";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there"));
+console.log(countAndDescribe(["sports", "cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
